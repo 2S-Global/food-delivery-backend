27afc8fc6cbe9b9ee85866363d5c0ba79249bf58
2S-Global <2sglobaltechnologies@gmail.com>
2025-11-20 17:33:59 +0530

    Implement list-delivery-boys API

--- controllers/userDataController.js
+++ controllers/userDataController.js
@@ -78,3 +78,53 @@ export const deleteCustomer = async (req, res) => {
     });
   }
 };
+
+// List delivery boys
+export const listDeliveryBoys = async (req, res) => {
+  try {
+    // Fetch all users with role 1 and not deleted
+    const candidates = await User.find({
+      role: 2,
+      is_del: false
+    }).select("name email phone_number is_active createdAt");
+
+    // If no data found
+    if (!candidates.length) {
+      return res.status(404).json({
+        success: false,
+        message: "No candidates found"
+      });
+    }
+
+    // Format date + time to: 22/05/2025, 01:58 PM
+    const formattedCandidates = candidates.map((item) => {
+      const formattedDate = item.createdAt.toLocaleString("en-GB", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+      });
+
+      return {
+        ...item._doc,
+        createdAt: formattedDate,
+      };
+    });
+
+    // Success response
+    return res.status(200).json({
+      success: true,
+      message: "Candidates retrieved successfully",
+      data: formattedCandidates
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error fetching candidates",
+      error: error.message
+    });
+  }
+};
--- routes/userDataRoutes.js
+++ routes/userDataRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import multer from "multer";
 import userAuth from "../middleware/authMiddleware.js";
 import adminMiddleware from "../middleware/adminMiddleware.js";
-import { toggleStatus, deleteCustomer } from "../controllers/userDataController.js"
+import { toggleStatus, deleteCustomer, listDeliveryBoys } from "../controllers/userDataController.js"
 
 // Initialize router
 const userDataRouter = express.Router();
@@ -14,5 +14,7 @@ const upload = multer({ storage: storage });
 // Login user
 userDataRouter.post("/toggle-status", upload.none(), userAuth, adminMiddleware, toggleStatus);
 userDataRouter.post("/delete-customer", upload.none(), userAuth, adminMiddleware, deleteCustomer);
+// List all delivery boys
+userDataRouter.get("/list-delivery-boys", userAuth, adminMiddleware, listDeliveryBoys);
 
 export default userDataRouter;
