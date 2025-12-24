5a1ffed2b1971a10d391a0856a613a1315ac34c8
2S-Global <2sglobaltechnologies@gmail.com>
2025-12-24 11:33:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ app.use(
     credentials: true,
   })
 );
+
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -52,3 +59,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 
 });
+
