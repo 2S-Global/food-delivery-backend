1639a1f5c77a6905b2bdae86b32ae99c70a261e0
Chandra-Sarkar <chandrasarkar26345@gmail.com>
2025-12-30 12:19:25 +0530

    Comment out server listening code in server.js

--- server.js
+++ server.js
@@ -61,10 +61,10 @@ app.use("/api/user-order", userOrderRouter);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
 
-});
+// });
 
 
 export default app;
