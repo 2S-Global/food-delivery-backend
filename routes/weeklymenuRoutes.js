7be6e34bc268740d1725c4d9f9a80fd005c0a125
2S-Global <2sglobaltechnologies@gmail.com>
2025-12-31 16:20:49 +0530

    update

--- routes/weeklymenuRoutes.js
+++ routes/weeklymenuRoutes.js
@@ -5,11 +5,11 @@ import {
   getAllWeeklyMenus,
   getMenuByDateAndUser
 } from "../controllers/weeklymenuController.js";
-
+import userAuth from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/weekly-menu", submitWeeklyMenu);
 router.get("/weekly-menu", getWeeklyMenuByDate); // ?date=YYYY-MM-DD
 router.get("/weekly-menu/all", getAllWeeklyMenus);       // Get single survey by ID
-router.get("/get-menu-by-date", getMenuByDateAndUser); // ?date=YYYY-MM-DD
+router.get("/get-menu-by-date",userAuth, getMenuByDateAndUser); // ?date=YYYY-MM-DD
 export default router;
