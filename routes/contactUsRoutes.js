93ac73be8b2c52a39f73587f2d293c0fee38c454
2S-Global <2sglobaltechnologies@gmail.com>
2026-01-01 18:42:01 +0530

    Simplify contactUsRoutes by removing admin endpoints
    Removed admin routes for getting and updating contact submissions.

--- routes/contactUsRoutes.js
+++ routes/contactUsRoutes.js
@@ -1,8 +1,6 @@
 import express from "express";
 import {
   submitContactUs,
-  getAllContactUs,
-  updateContactStatus,
 } from "../controllers/contactUsController.js";
 
 import adminAuth from "../middleware/adminMiddleware.js";
@@ -11,9 +9,5 @@ const router = express.Router();
 
 // Public
 router.post("/", submitContactUs);
-
-// Admin
-router.get("/admin", adminAuth, getAllContactUs);
-router.patch("/admin/:id", adminAuth, updateContactStatus);
 
 export default router;
